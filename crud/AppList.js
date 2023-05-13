b80f6c0e3fa7f78f8aec99f47f6965058c0c72c6
lcffreitas <lcffreitas1@gmail.com>
2023-05-13 14:08:57 -0300

    módulo 1 ao 3

--- crud/AppList.js
+++ crud/AppList.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import AppItem from './AppItem';
+import Database from './Database';
 
-export default function AppList() {
+export default function AppList({ route, navigation }) {
+
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+      Database.getItems().then(items => setItems(items));
+  }, [route]);
+
   return (
     <View style={styles.container}>
-      <Text>List!</Text>
       <StatusBar style="light" />
+      <Text style={styles.title}>Lista de Compras</Text>
+      <ScrollView
+        style={styles.scrollContainer}
+        contentContainerStyle={styles.itemsContainer}
+      >
+        {items.map((item) => {
+          return (
+            <AppItem
+              key={item.id}
+              id={item.id}
+              item={item.quantidade + "  de " + item.descricao}
+              navigation={navigation}
+            />
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -16,6 +40,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#D93600',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 20
+  },
+  scrollContainer: {
+    flex: 1,
+    width: '90%'
+  },
+  itemsContainer: {
+    flex: 1,
+    marginTop: 10,
+    padding: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    alignItems: 'stretch',
+    backgroundColor: '#fff'
   },
 });
